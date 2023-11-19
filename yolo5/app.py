3f493d3b6dcaceac0444e7574fd84251e07ce221
Ezdehar-Jayosi <ezdeharj.95@gmail.com>
2023-11-19 09:37:46 +0200

    added some lines that were removed by mistake

--- yolo5/app.py
+++ yolo5/app.py
@@ -58,7 +58,83 @@ def teardown_appcontext(exception=None):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Rest of the code remains unchanged
+    # Generates a UUID for this current prediction HTTP request. This id can be used as a reference in logs to identify and track individual prediction requests.
+    prediction_id = str(uuid.uuid4())
+
+    logger.info(f'prediction: {prediction_id}. start processing')
+
+    # Receives a URL parameter representing the image to download from S3
+    img_name = request.args.get('imgName')
+
+    # TODO download img_name from S3, store the local image path in original_img_path
+    #  The bucket name should be provided as an env var BUCKET_NAME.
+    original_img_path = f'{img_name}'
+    try:
+        # Download the image from S3
+        s3.download_file(images_bucket, img_name, original_img_path)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            logger.error("The image does not found")
+        else:
+            logger.error(e)
+            raise
+    logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
+
+    # Predicts the objects in the image
+    run(
+        weights='yolov5s.pt',
+        data='data/coco128.yaml',
+        source=original_img_path,
+        project='static/data',
+        name=prediction_id,
+        save_txt=True
+    )
+
+    logger.info(f'prediction: {prediction_id}/{original_img_path}. done')
+
+    # This is the path for the predicted image with labels
+    # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
+    predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path}')
+
+    # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
+    # s3_prediction_img_path = f'{img_name.split(".")[0]}_prediction.jpg'
+    try:
+        s3.upload_file(str(predicted_img_path), images_bucket, f'{img_name.split(".")[0]}_prediction.jpg')
+    except Exception as e:
+        logger.error(e)
+        raise
+        return
+
+    # Parse prediction labels and create a summary
+    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
+    if pred_summary_path.exists():
+        with open(pred_summary_path) as f:
+            labels = f.read().splitlines()
+            labels = [line.split(' ') for line in labels]
+            labels = [{
+                'class': names[int(l[0])],
+                'cx': float(l[1]),
+                'cy': float(l[2]),
+                'width': float(l[3]),
+                'height': float(l[4]),
+            } for l in labels]
+
+        logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
+
+        prediction_summary = {
+            'prediction_id': prediction_id,
+            'original_img_path': original_img_path,
+            'predicted_img_path': predicted_img_path,
+            'labels': labels,
+            'time': time.time()
+        }
+
+        # TODO store the prediction_summary in MongoDB
+        collection.insert_one(prediction_summary)
+        return prediction_summary
+    else:
+        return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
