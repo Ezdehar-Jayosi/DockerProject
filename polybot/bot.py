571fd14473a5e89537af0ded8e2068c27bba7017
Ezdehar-Jayosi <ezdeharj.95@gmail.com>
2023-11-20 21:28:05 +0200

    Finished pobot imp, before testing

--- polybot/bot.py
+++ polybot/bot.py
@@ -1,8 +1,12 @@
+import boto3
 import telebot
 from loguru import logger
 import os
 import time
 from telebot.types import InputFile
+import argparse
+import subprocess
+import requests
 
 
 class Bot:
@@ -74,36 +78,80 @@ class QuoteBot(Bot):
 
 
 class ObjectDetectionBot(Bot):
-   def handle_message(self, msg):
-       logger.info(f'Incoming message: {msg}')
+    def __init__(self, token, telegram_chat_url):
+        Bot.__init__(self, token, telegram_chat_url)
 
-       if self.is_current_msg_photo(msg):
-           img_path = self.download_user_photo(msg)
+        self.Bucket_Name = os.environ['BUCKET_NAME']
+        self.aws_region = os.environ['REGION']
+        # self.s3_client = boto3.client('s3', aws_access_key_id=self.s3_access_key, aws_secret_access_key=self.s3_secret_key)
+        self.s3_resource = boto3.resource(
+            's3',
+            region_name=self.aws_region,
+            aws_access_key_id=os.environ['S3_ACCESS_KEY'],
+            aws_secret_access_key=os.environ['S3_SECRET_KEY']
+        )
 
-           # TODO: Upload the photo to S3
-           s3_url = self.upload_to_s3(img_path)
+    def format_prediction_results(self, prediction_result):
+        # Extract relevant information from the prediction result
+        prediction_id = prediction_result["prediction_id"]
+        labels = prediction_result["labels"]
 
-           # TODO: Send a request to the `yolo5` service for prediction
-           prediction_result = self.send_yolo5_request(s3_url)
+        # Create a dictionary to store the count of each detected object
+        object_counts = {}
 
-           # TODO: Send results to the Telegram end-user
-           self.send_text(msg['chat']['id'], f'Object Detection Results: {prediction_result}')
+        # Iterate over each label in the prediction result
+        for label in labels:
+            class_name = label["class"]
+            object_counts[class_name] = object_counts.get(class_name, 0) + 1
 
-   def upload_to_s3(self, img_path):
-       # TODO: Implement the logic to upload the image to S3
-       # Example: You can use a library like boto3 to upload the image to your S3 bucket
-       # Replace the placeholders with your actual S3 credentials and bucket information
-       # s3.upload_file(img_path, 'your_bucket_name', 'your_s3_key')
+        # Convert the object counts dictionary to a formatted string
+        formatted_results = ", ".join(f"{obj}: {count}" for obj, count in object_counts.items())
 
-       # Placeholder: Return a dummy S3 URL for demonstration purposes
-       return f'https://your-s3-bucket.s3.amazonaws.com/{os.path.basename(img_path)}'
+        return f"Detected objects: {formatted_results}"
 
-   def send_yolo5_request(self, s3_url):
-       # TODO: Implement the logic to send a request to the yolo5 service
-       # Example: You can use the requests library to perform an HTTP POST request
-       # Replace the URL with the actual endpoint of your yolo5 service
-       yolo5_url = 'http://yolo5-service-endpoint/predict'
-       response = requests.post(yolo5_url, json={'s3_url': s3_url})
 
-       # Placeholder: Return a dummy prediction result for demonstration purposes
-       return response.json()['predictions']
+def handle_message(self, msg):
+    logger.info(f'Incoming message: {msg}')
+
+    if self.is_current_msg_photo(msg):
+        img_path = self.download_user_photo(msg)
+
+        # Upload the photo to S3
+        s3_url = self.upload_to_s3(img_path)
+
+        # Send a request to the `yolo5` service for prediction
+        prediction_result = self.send_yolo5_request(s3_url)
+
+        # Format the prediction results for the PolyBot
+        formatted_results = self.format_prediction_results(prediction_result)
+
+        # Send the formatted results to the Telegram end-user
+        self.send_text(msg['chat']['id'], formatted_results)
+
+
+def upload_to_s3(self, img_path):
+    # TODO: Implement the logic to upload the image to S3
+    # Example: You can use a library like boto3 to upload the image to your S3 bucket
+    # Replace the placeholders with your actual S3 credentials and bucket information
+    # s3.upload_file(img_path, 'your_bucket_name', 'your_s3_key')
+    try:
+        self.s3_resource.Bucket(self.Bucket_Name).put_object(
+            Key=os.path.basename(img_path),
+            Body=open(img_path, 'rb')
+        )
+    except Exception as e:
+        logger.error(e)
+        raise
+        return
+    return os.path.basename(img_path)
+
+
+def send_yolo5_request(self, s3_url):
+    # TODO: Implement the logic to send a request to the yolo5 service
+    yolo5_url = 'http://yolo5:8081/predict'
+    response = requests.post(yolo5_url, json={'imgName': s3_url})
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
